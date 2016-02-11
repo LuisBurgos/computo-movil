f3e9b4bd2feb03aa0d8165c5129e52b30540fd84
Luis Burgos <luisarturo.burgos@gmail.com>
2016-02-11 13:27:40 -0600

    Fix some naming

--- students-database-example/app/src/main/java/com/luisburgos/studentsdatabase/utils/Injection.java
+++ students-database-example/app/src/main/java/com/luisburgos/studentsdatabase/utils/Injection.java
@@ -5,12 +5,28 @@ import android.support.annotation.NonNull;
 
 import com.luisburgos.studentsdatabase.data.StudentDataSource;
 
+import java.sql.SQLException;
+
 /**
  * Created by luisburgos on 2/02/16.
  */
 public class Injection {
 
     public static StudentDataSource provideStudentsDataSource(Context context) {
-        return new StudentDataSource(context);
+        StudentDataSource dataSource = new StudentDataSource(context);
+
+        //Dummy init block
+        /*try {
+            dataSource.open();
+            dataSource.insertStudent("10004020", "Luis", "Burgos");
+            dataSource.insertStudent("10003020", "Juan", "Mendez");
+            dataSource.insertStudent("99994020", "Elsy", "Garcia");
+            dataSource.insertStudent("10003000", "Jesus", "Perez");
+            dataSource.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }*/
+
+        return dataSource;
     }
 }
