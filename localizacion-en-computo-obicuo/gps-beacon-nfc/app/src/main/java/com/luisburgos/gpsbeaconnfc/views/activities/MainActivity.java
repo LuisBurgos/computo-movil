a0e6bad5ce2bcda5e3bbe228f53710032af7967e
Luis Burgos <luisarturo.burgos@gmail.com>
2016-04-26 14:40:45 -0500

    Add toolbar title

--- localizacion-en-computo-obicuo/gps-beacon-nfc/app/src/main/java/com/luisburgos/gpsbeaconnfc/views/activities/MainActivity.java
+++ localizacion-en-computo-obicuo/gps-beacon-nfc/app/src/main/java/com/luisburgos/gpsbeaconnfc/views/activities/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -55,6 +56,8 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ButterKnife.bind(this);
 
